161ad64092244cc451948f5a314129f08dd159be
Daniel <dani.v2509@outlook.com>
2024-10-16 17:04:55 +0200

    Solución

--- src/main/java/com/campusdual/classroom/Car.java
+++ src/main/java/com/campusdual/classroom/Car.java
@@ -2,4 +2,95 @@ package com.campusdual.classroom;
 
 public class Car {
 
+    public String brand;
+    public String model;
+    public static final int MAX_SPEED = 120;
+    public String fuel;
+
+    public int speedometer = 0;
+    public int tachometer = 0;
+    public String gear = "N";
+    public boolean reverse = false;
+    public int wheelsAngle = 0;
+
+    public Car(String brand, String model, String fuel) {
+        this.brand = brand;
+        this.model = model;
+        this.fuel = fuel;
+    }
+
+    public Car() {}
+
+    public void start() {
+        if (this.tachometer == 0) {
+            this.tachometer = 1000;
+            System.out.println("Vehículo encendido");
+        } else {
+            System.out.println("El vehículo ya está encendido");
+        }
+    }
+
+    public void stop() {
+        if (this.speedometer == 0) {
+            this.tachometer = 0;
+            System.out.println("Vehículo apagado");
+        } else {
+            System.out.println("No se puede apagar el vehículo, primero tiene que estar detenido");
+        }
+    }
+
+    public void accelerate() {
+        if (speedometer < 120) this.speedometer += 10;
+        if (this.speedometer < 120) System.out.print("..." + this.speedometer + "km/h ");
+        if (this.speedometer == 120) System.out.printf("\nVelocidad máxima");
+    }
+
+    public void brake() {
+        if (speedometer > 0) this.speedometer -= 10;
+        if (this.speedometer > 0) System.out.print(this.speedometer + "km/h" + "... ");
+        if (this.speedometer == 0) System.out.printf("\nVehículo detenido");
+    }
+
+    public void turnAngleOfWheels(int angle) {
+        if (angle < 0){
+            for (int i = 0; i < Math.abs(angle); i++) {
+                if (this.wheelsAngle > -45) this.wheelsAngle--;
+            }
+        }
+        else if (angle > 0){
+            for (int i = 0; i < angle; i++) {
+                if (this.wheelsAngle < 45) this.wheelsAngle++;
+            }
+        }
+        System.out.println("Volante a " + this.wheelsAngle + " grados");
+    }
+
+    public boolean isTachometerGreaterThanZero(){
+        return this.tachometer > 0;
+    }
+
+    public boolean isTachometerEqualToZero(){
+        return this.tachometer == 0;
+    }
+
+    public boolean isReverse() {
+        return reverse;
+    }
+
+    public void setReverse(boolean reverse) {
+        if (speedometer == 0){
+            this.reverse = reverse;
+            if (reverse == true){
+                this.gear = "R";
+            }
+            else {
+                this.gear = "N";
+            }
+        }
+    }
+
+    public void showDetails() {}
+
+    public static void main(String[] args) {}
 }
+
--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -3,6 +3,82 @@ package com.campusdual.classroom;
 public class Exercise12 {
     public static void main(String[] args) {
 
+        // Creamos el objeto del coche
+        Car mazda5 = new Car("Mazda", "5", "Diesel");
+
+// Un método booleano que verifique si el valor del tacómetro es cero
+        System.out.println("Detenido: " + isStopped(mazda5));
+
+// Encender el coche comprobando el valor correcto del tacómetro (investigar cuál es el valor correcto)
+        mazda5.start();
+
+// Encender el coche cuando ya está encendido (comprobar que no se puede encender si ya está encendido)
+        mazda5.start();
+
+// Apagar el coche comprobando que el valor del tacómetro es correcto
+        mazda5.stop();
+
+// Acelerar el coche
+        System.out.println("ACELERANDO: ");
+        for (int i = 0; i < 5; i++) {
+            mazda5.accelerate();
+        }
+        System.out.println();
+
+// Apagar el coche mientras todavía está en movimiento (y comprobar que no se puede)
+        mazda5.stop();
+
+// Acelerar hasta intentar superar la velocidad máxima (y comprobar que no se puede)
+        System.out.println("ACELERANDO AL MÁXIMO:");
+        for (int i = 0; i < 26; i++) {
+            mazda5.accelerate();
+        }
+        System.out.println();
+
+// Frenar la velocidad
+        System.out.println("FRENANDO:");
+        for (int i = 0; i < 8; i++) {
+            mazda5.brake();
+        }
+        System.out.println();
+
+// Frenar hasta un valor negativo (y comprobar que no se puede)
+        System.out.println("FRENANDO AL MÍNIMO: ");
+        for (int i = 0; i < 5; i++) {
+            mazda5.brake();
+        }
+
+// Girar el volante 20 grados
+        System.out.println("GIRANDO 20 GRADOS: ");
+        mazda5.turnAngleOfWheels(20);
+
+// Girar el volante más de 45 grados ó -45 grados (y comprobar que no se puede)
+        System.out.println("PROBANDO LÍMITES DEL VOLANTE:");
+        mazda5.turnAngleOfWheels(60);
+        mazda5.turnAngleOfWheels(-100);
+
+// Detener el coche y poner marcha atrás
+        System.out.println("PONIENDO EL COCHE MARCHA ATRÁS");
+        mazda5.setReverse(true);
+        System.out.println("Marcha atrás: " + mazda5.isReverse());
+
+// Poner marcha atrás mientras el coche está en movimiento marcha adelante (y comprobar que no se puede)
+        System.out.println("RESTAURANDO MARCHA ADELANTE: ");
+        mazda5.setReverse(false);
+        System.out.println("Marcha atrás: " + mazda5.isReverse());
+        System.out.println("ACELERANDO, Y PROBANDO A METER MARCHA ATRÁS:");
+        for (int i = 0; i < 35; i++) {
+            mazda5.accelerate();
+        }
+        System.out.println();
+        System.out.println("PRUEBO A METER MARCHA ATRÁS EN MOVIMIENTO: ");
+        mazda5.setReverse(true);
+        System.out.println("Marcha atrás: " + mazda5.isReverse());
+
+    }
+
+        static boolean isStopped(Car car){
+        return car.speedometer == 0;
     }
 
 }
